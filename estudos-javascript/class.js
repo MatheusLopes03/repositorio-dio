b40054d45113c0c1c8929edee3ba800a989c1b6e
MatheusLopes03 <lopesalves.ma@gmail.com>
2022-11-11 18:09:30 -0300

    estudos de class e objetos

---
+++ estudos-javascript/class.js
@@ -0,0 +1,73 @@
+/* 
+1- Crie uma classe para representar carros. 
+Os carros possuem uma marca, uma cor e um gasto médio de combustível por Kilômetro rodado.
+Crie um método que dado a quantidade de quilómetros e o preço do combustível nos dê o valor 
+gasto em reais para realizar este percurso.
+*/
+
+class Carro {
+    marca;
+    cor;
+    gastoPorKm;
+
+    constructor(marca, cor, gastoPorKm) {
+        this.marca = marca;
+        this.cor = cor;
+        this.gastoPorKm = gastoPorKm;
+    }
+
+    calcularGastoPercurso(distanciaKm, precoCombustivel){
+        return distanciaKm * this.gastoPorKm * precoCombustivel;
+    }
+
+}
+
+const uninhotunado = new Carro('Fiat', 'Vermelho', 1/13);
+console.log(uninhotunado);
+console.log(uninhotunado.calcularGastoPercurso(73, 5));
+
+
+/*
+2- Crie uma classe para representar pessoas.
+Para cada pessoa teremos os atributos nome, peso e altura.
+As pessoas devem ter a capacidade de dizer o valor do seu IMC (IMC = peso / (altura * altura));
+Instancie uma pessoa chamada José que tenha 70kg de peso e 1,75 de altura e peça ao José para dizer o valor
+do seu IMC;
+*/
+
+class Pessoa {
+    nome;
+    peso;
+    altura;
+
+    constructor(nome, peso, altura) {
+        this.nome = nome;
+        this.peso = peso;
+        this.altura = altura;
+    }
+
+    calcularImc(){
+        return this.peso / (this.altura * this.altura);
+    }
+    classificarImc(){
+        const imc = this.calcularImc();
+
+        if (imc > 40) {
+            return ('Obesidade Grave');
+        } else if (imc >= 30) {
+            return ('Obeso');
+        } else if (imc >= 25) {
+            return ('Acima do peso');
+        } else if (imc >= 18.5) {
+            return ('Peso nomal');
+        } else {
+            return ('Abaixo do peso');
+        }
+    }
+}
+
+const jose = new Pessoa('José', 70, 1.75);
+console.log(jose);
+console.log(jose.classificarImc());
+
+
---
+++ estudos-javascript/condicoes-funcoes.js
@@ -0,0 +1,51 @@
+
+/* Terceiro exercicio implementando funções
+
+3 - Elabore um algoritmo que calcule o que deve ser pago por um produto, considerando o preço normal de etiqueta e a escolha da condição de pagamento. 
+Utilize os códigos da tabela a seguir para ler qual a condição de pagamento escolhida e efetuar o cálculo adequado.
+
+Código Condição de pagamento:
+- À vista Débito, recebe 10% de desconto;
+- À vista no Dinheiro ou PIX, recebe 15% de desconto;
+- Em duas vezes, preço normal de etiqueta sem juros;
+- Acima de duas vezes, preço normal de etiqueta mais juros de 10%;
+*/
+
+function aplicarDesconto (valor, desconto) {
+    return (valor - (valor * (desconto / 100)));
+}
+
+function aplircarJuros (valor, juros) {
+    return (valor + (valor * (juros / 100)));
+}
+
+const precoEtiqueta = 100;
+const formaPagamento = 4;
+
+if (formaPagamento === 1) {
+    console.log(aplicarDesconto(precoEtiqueta, 10));
+} else if (formaPagamento === 2) {
+    console.log(aplicarDesconto(precoEtiqueta, 15))
+} else if (formaPagamento === 3){
+    console.log (precoEtiqueta);
+} else {
+    console.log (aplircarJuros (precoEtiqueta, 10));
+}
+
+//Função para verfircar nome e maioridade.
+
+function escrevaMeuNome(nome) {
+    console.log('Meu nome é ' + nome);
+}
+
+escrevaMeuNome('Matheus');
+
+function verificarIdade (idade) {
+    if(idade >= 18) {
+        console.log('Maior de idade')
+    } else {
+        console.log('Menor de idade')
+    }
+}
+
+verificarIdade(19);
